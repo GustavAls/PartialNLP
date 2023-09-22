31a5d127e859e6ed846bff985264ab2362b7f568
PeterKampen <s183993@student.dtu.dk>
2023-09-22 09:44:42 +0200

    Laplace correct and better swag

--- Laplace/uci_laplace.py
+++ Laplace/uci_laplace.py
@@ -15,6 +15,7 @@ from uci import UCIDataloader, UCIDataset, UCIBostonDataset, UCIEnergyDataset, U
 from torch.nn import MSELoss
 from torch.distributions import Gamma
 
+
 def count_parameters(model):
     """Count the number of parameters in a model.
         Args:
@@ -38,6 +39,7 @@ def calculate_mse(preds, labels):
     print("Loss using means is equal to", base_pred)
     return base_pred
 
+
 def calculate_nll(preds, labels, var, y_scale, y_loc):
     """Calculate the negative log likelihood of the predictions.
         Args:
@@ -50,12 +52,13 @@ def calculate_nll(preds, labels, var, y_scale, y_loc):
     results = []
     scales = var
     for pred, scale, label in zip(preds, scales, labels):
-        dist = Normal(pred*y_scale + y_loc, scale*y_scale)
-        results.append(dist.log_prob(label*y_scale + y_loc))
+        dist = Normal(pred * y_scale + y_loc, scale * y_scale)
+        results.append(dist.log_prob(label * y_scale + y_loc))
     nll = -1 * sum(results) / len(results)
     return nll
 
-def calculate_precision_from_prior(residuals, alpha = 3, beta = 5):
+
+def calculate_precision_from_prior(residuals, alpha=3, beta=5):
     """
 
     :param residuals: torch.tensor of form (predictions - labels)
@@ -65,24 +68,57 @@ def calculate_precision_from_prior(residuals, alpha = 3, beta = 5):
     """
 
     residuals = residuals.flatten()
-    mean_r = torch.mean(residuals)
+    mean_r = 0
     n = len(residuals)
-    dist = Gamma(alpha + n/2, beta + 1/2 * torch.sum((residuals - 0)**2))
+    dist = Gamma(alpha + n / 2, beta + 1 / 2 * torch.sum((residuals - mean_r) ** 2))
     posterior_tau = torch.mean(dist.sample((1000,)))
     return posterior_tau
 
-def calculate_variance(model, batch, labels, alpha = 3, beta = 5):
+
+def calculate_variance(model, batch, labels, alpha=3, beta=5):
     preds, _ = model(batch)
 
     residuals = preds.flatten() - labels.flatten()
 
     precision = calculate_precision_from_prior(residuals, alpha, beta)
-    variance = 1/precision
+    variance = 1 / precision
     return variance
+
 
 def predict(val_data, model):
     return model(val_data)
 
+
+def calculate_nll_potential_final(f_mu, f_std, labels, sigma, num_samples=100, y_scale=1, y_loc=1):
+    results = []
+    for pred, lab in zip(f_mu, labels):
+        dist_sampler = Normal(f_mu, f_std)
+        results_tmp = []
+        for sample in dist_sampler.sample((num_samples,)):
+            d = Normal(sample, sigma)
+            results_tmp.append(d.log_prob(lab))
+
+        results.append(np.mean(results_tmp))
+    return -1 * np.mean(results)
+
+
+def find_best_prior(ml_model, subnetwork_indices, train_dataloader, val_loader, y_scale, y_loc):
+    prior_precision_sweep = np.logspace(0, 5, num=10, endpoint=True)
+    batch, label = next(iter(val_loader))
+    results = []
+    for prior in prior_precision_sweep:
+        la = Laplace(copy.deepcopy(ml_model), 'regression',
+                     subset_of_weights='subnetwork',
+                     hessian_structure='full',
+                     subnetwork_indices=subnetwork_indices,
+                     prior_precision=torch.tensor([float(prior)]))
+
+        la.fit(train_dataloader)
+        f_mu, f_var = la(batch)
+        f_var = f_var.detach().squeeze().sqrt()
+        results.append(calculate_nll(f_mu.flatten(), label.flatten(), f_var, y_scale, y_loc))
+
+    return prior_precision_sweep[np.argmin(results)]
 
 
 def run_percentiles(mle_model, train_dataloader, dataset, percentages):
@@ -104,34 +140,55 @@ def run_percentiles(mle_model, train_dataloader, dataset, percentages):
     test_nll = []
     val_mse = []
     test_mse = []
+    y_scale = torch.from_numpy(dataset.scl_Y.scale_)
+    y_loc = torch.from_numpy(dataset.scl_Y.mean_)
     for p in percentages:
         ml_model = copy.deepcopy(mle_model)
         subnetwork_mask = LargestMagnitudeSubnetMask(ml_model, n_params_subnet=int((p / 100) * num_params))
         subnetwork_indices = subnetwork_mask.select()
 
+        counter = 0
+        while counter < 50:
+            try:
+                best_prior = find_best_prior(mle_model, subnetwork_indices, train_dataloader,
+                                             DataLoader(UCIDataloader(dataset.X_val, dataset.y_val, ),
+                                                        batch_size=dataset.X_val.shape[0]),
+                                             y_scale=y_scale, y_loc=y_loc)
+                break
+            except:
+                counter += 1
+
         # Define and fit subnetwork LA using the specified subnetwork indices
         la = Laplace(ml_model, 'regression',
                      subset_of_weights='subnetwork',
                      hessian_structure='full',
-                     subnetwork_indices=subnetwork_indices)
+                     subnetwork_indices=subnetwork_indices,
+                     prior_precision=torch.tensor([float(best_prior)]))
+
+        print('Best prior was', best_prior)
         la.fit(train_dataloader)
-        la.optimize_prior_precision()
-        y_scale = torch.from_numpy(dataset.scl_Y.scale_)
-        y_loc = torch.from_numpy(dataset.scl_Y.mean_)
+
         batch, labels = next(iter(train_dataloader))
 
-        sigma_noise = calculate_variance(la, batch, labels, alpha = 3, beta = 1)
+        sigma_noise = calculate_variance(la, batch, labels, alpha=3, beta=1)
         val_targets = torch.from_numpy(dataset.y_val).to(torch.float32)
         val_preds_mu, val_preds_var = la(torch.from_numpy(dataset.X_val).to(torch.float32))
         val_preds_sigma = val_preds_var.squeeze().sqrt()
-        predictive_std_val = torch.sqrt(val_preds_sigma**2 + sigma_noise)
+        predictive_std_val = torch.sqrt(val_preds_sigma ** 2)
 
         test_targets = torch.from_numpy(dataset.y_test).to(torch.float32)
         test_preds_mu, test_preds_var = la(torch.from_numpy(dataset.X_test).to(torch.float32))
         test_preds_sigma = test_preds_var.squeeze().sqrt()
-        predictive_std_test = torch.sqrt(test_preds_sigma ** 2 + sigma_noise)
+        predictive_std_test = torch.sqrt(test_preds_sigma ** 2)
         val_targets = val_targets.flatten()
         test_targets = test_targets.flatten()
+        if p == percentages[0]:
+            val_mse.append(calculate_mse(val_preds_mu.flatten(), val_targets))
+            val_nll.append(calculate_nll(
+                val_preds_mu.flatten(), val_targets, torch.tile(sigma_noise, (len(val_targets),)), y_scale, y_loc))
+            test_mse.append(calculate_mse(val_preds_mu.flatten(), val_targets))
+            test_nll.append(calculate_nll(
+                test_preds_mu.flatten(), test_targets, torch.tile(sigma_noise, (len(test_targets),)), y_scale, y_loc))
 
         val_mse.append(calculate_mse(val_preds_mu.flatten(), val_targets))
         val_nll.append(calculate_nll(val_preds_mu.flatten(), val_targets, predictive_std_val, y_scale, y_loc))
@@ -175,7 +232,8 @@ def multiple_runs(data_path, dataset_class, num_runs, device, num_epochs, output
         train_dataloader = DataLoader(UCIDataloader(dataset.X_train, dataset.y_train), batch_size=n_train)
         val_dataloader = DataLoader(UCIDataloader(dataset.X_val, dataset.y_val), batch_size=n_val)
 
-        mle_model = train(network=mle_model, dataloader_train=train_dataloader, dataloader_val=val_dataloader, **train_args)
+        mle_model = train(network=mle_model, dataloader_train=train_dataloader, dataloader_val=val_dataloader,
+                          **train_args)
         val_nll, test_nll, val_mse, test_mse = run_percentiles(mle_model, train_dataloader, dataset, percentages)
 
         save_name = os.path.join(output_path, f'results_{run}_{dataset_class.dataset_name}.pkl')
@@ -198,6 +256,7 @@ def multiple_runs(data_path, dataset_class, num_runs, device, num_epochs, output
         all_res.append(results)
     return all_res
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument("--output_path", type=str, default=os.getcwd())
@@ -218,8 +277,7 @@ if __name__ == "__main__":
     else:
         dataset_class = UCIYachtDataset
 
-    results = multiple_runs(args.data_path, dataset_class, args.num_runs, args.device, args.num_epochs, args.output_path)
+    results = multiple_runs(args.data_path, dataset_class, args.num_runs, args.device, args.num_epochs,
+                            args.output_path)
     breakpoint()
     print("Laplace experiments finished!")
-
-
