e0ee02209239ef023462526883db9958bb753c91
GustavAls <gustavals1998@gmail.com>
2023-10-09 17:22:57 +0200

    Swag Laplace

--- Visualization/plot_uci.py
+++ Visualization/plot_uci.py
@@ -7,48 +7,72 @@ import torch.nn as nn
 import pickle
 import os
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
 
 
-def plot_errorbar_percentages(df, errorbar_func, estimator=None, ax=None, y='la_nll'):
+def plot_errorbar_percentages(df, errorbar_func, estimator=None, ax=None, y='Laplace_nll', color_scheme_1=True):
+    # cmap = get_cmap(20)
+    # color_num = np.random.randint(2, 18)
+    # color_map = mpl.colormaps['Set1']
+
+    map_color = 'tab:red'
+    stochastic_color = 'tab:green'
+    point_err_color = 'tab:blue' if color_scheme_1 else 'tab:orange'
+
     sns.pointplot(errorbar=errorbar_func,
                   data=df, x="percentages", y=y,
-                  join=False,
-                  markers="d", scale=.5, err_kws={'linewidth': 0.5}, estimator=estimator,
+                  join=False, capsize=.25,
+                  markers="d", scale=1.0, err_kws={'linewidth': 0.7}, estimator=estimator,
+                  color=point_err_color,
+                  label=y.split('_')[0],
                   ax=ax)
 
     nll_array = np.array(df[y]).reshape((-1, 10))
     estimated = estimator(nll_array, axis=0)
     map_val = estimated[0]
-    ax.axhline(y=map_val, linestyle='--', linewidth=1, alpha=0.7, label=None)
+    ax.axhline(y=map_val, linestyle='--', linewidth=1, alpha=0.7,
+               color=map_color, label='MAP' if not color_scheme_1 else '_nolegend_')
     fully_stochastic_val = estimated[-1]
-    ax.axhline(y=fully_stochastic_val, linestyle='--', linewidth=1, alpha=0.7, label=None)
+    ax.axhline(y=fully_stochastic_val, linestyle='--', linewidth=1, alpha=0.7,
+               color=stochastic_color, label='100% Stochastic' if not color_scheme_1 else '_nolegend_')
 
     # Adjust layout
     plt.tight_layout()
 
 
 def plot_estimator(df, errorbar_func, estimator=None, ax=None, data_name=None):
-    legend_labels = []
-    for key in df:
+    method_names = []
+    np.random.seed(42)
+    color_scheme_1 = True
+    for i, key in enumerate(df):
         if 'percentages' not in key:
-            legend_labels.append(key.split('_')[0])
+            method_names.append(key.split('_')[0])
             plot_errorbar_percentages(df=df,
                                       errorbar_func=errorbar_func,
                                       estimator=estimator,
                                       y=key,
-                                      ax=ax)
+                                      ax=ax,
+                                      color_scheme_1=color_scheme_1)
+            color_scheme_1 = not color_scheme_1
 
-    ax.set_title("Laplace Swag" + " - " + estimator.__name__ + " - " + data_name, fontsize=12, pad=-20)
+    title = method_names[0] + " & " + method_names[1] + " - " + estimator.__name__ + " - " + data_name
+    ax.set_title(label=title, fontsize=12, pad=-20)
 
     # Set labels and legend
     ax.set_xlabel("Percentages", fontsize=12)
     ax.set_ylabel("nll", fontsize=12)
-    # ax.legend(title="Methods", labels=legend_labels, fontsize=10)
+    ax.legend(fontsize=10)
 
     # Customize the grid appearance
-    ax.grid(axis='y', linestyle='-', alpha=0.3)
+    # ax.grid(axis='y', linestyle='-', alpha=0.3)
     # ax.grid(b=True, which='major', color='w', linewidth=1.0)
-
+    # Set the Seaborn style
+    plt.savefig(os.path.join(os.getcwd(), title + '.png'))
     plt.show(block=False)
 
 
@@ -57,9 +81,9 @@ def plot_partial_percentages(percentages, res, data_name=None, df=None, num_runs
         df = pd.DataFrame()
         df['percentages'] = num_runs * percentages
         for key, val in res.items():
-            if 'la' in key:
+            if 'Laplace' in key:
                 df[key + '_nll'] = [nll.item() for nll in val.flatten()]
-            elif 'swag' in key:
+            elif 'SWAG' in key:
                 df[key + '_nll'] = [(-1) * ll for ll in val.flatten()]
     fig, ax = plt.subplots(1, 1)
     # plt.rcParams['figure.figsize'] = [10, 8]
@@ -120,6 +144,33 @@ def read_data_swag_la(path, include_map = True):
     return percentages, test_nll, test_mse
 
 
+def read_data_swag_la_combined(path, include_map=True):
+    percentages = []
+    test_nll_la = []
+    test_mse_la = []
+    test_nll_swag = []
+    test_mse_swag = []
+    for p in os.listdir(path):
+        if 'results' in p:
+            pcl = pickle.load(open(os.path.join(path, p), 'rb'))
+            if 'laplace' in p:
+                test_nll_la.append(pcl['test_nll'])
+                percentages.append(pcl['percentages'])
+                test_mse_la.append([i.item() if not isinstance(i, float) else i for i in pcl['test_mse']][1:])
+            else:
+                test_nll_swag.append(pcl['test_nll'])
+                test_mse_swag.append([i.item() if not isinstance(i, float) else i for i in pcl['test_mse']][1:])
+
+    percentages = percentages[-1]
+    if include_map:
+        percentages = [0] + percentages
+    test_nll_la = np.array(test_nll_la)
+    test_mse_la = np.array(test_mse_la)
+    test_nll_swag = np.array(test_nll_swag)
+    test_mse_swag = np.array(test_mse_swag)
+    return percentages, test_nll_la, test_mse_la, test_nll_swag, test_mse_swag
+
+
 def read_vi_data(path):
 
     percentages = []
@@ -177,6 +228,7 @@ def get_under_folders_and_names(path):
             paths.append(os.path.join(path, p))
     return names, paths
 
+
 def plot_hmc_vi(path_hmc, path_vi):
     names, paths_hmc = get_under_folders_and_names(path_hmc)
     _, paths_vi = get_under_folders_and_names(path_vi)
@@ -186,7 +238,7 @@ def plot_hmc_vi(path_hmc, path_vi):
         percentages, test_nll_la, test_mse_la = read_data_swag_la(p_la, include_map=True)
         _, test_nll_swag, test_mse_swag = read_data_swag_la(p_swag, include_map=True)
         plot_partial_percentages(percentages=percentages,
-                                 res={'la': test_nll_la, 'swag': test_nll_swag},
+                                 res={'Laplace': test_nll_la, 'SWAG': test_nll_swag},
                                  data_name=name,
                                  num_runs=test_nll_la.shape[0])
 
@@ -200,15 +252,33 @@ def plot_la_swag(path_la, path_swag):
         percentages, test_nll_la, test_mse_la = read_data_swag_la(p_la, include_map=True)
         _, test_nll_swag, test_mse_swag = read_data_swag_la(p_swag, include_map=True)
         plot_partial_percentages(percentages=percentages,
-                                 res={'la': test_nll_la, 'swag': test_nll_swag},
+                                 res={'Laplace': test_nll_la, 'SWAG': test_nll_swag},
+                                 data_name=name,
+                                 num_runs=test_nll_la.shape[0])
+
+
+def plot_la_swag_combined(path):
+    names, paths = get_under_folders_and_names(path)
+
+    for name, p in zip(names, paths):
+        (percentages,
+         test_nll_la,
+         test_mse_la,
+         test_nll_swag,
+         test_mse_swag) = read_data_swag_la_combined(p, include_map=True)
+        plot_partial_percentages(percentages=percentages,
+                                 res={'Laplace': test_nll_la, 'SWAG': test_nll_swag},
                                  data_name=name,
                                  num_runs=test_nll_la.shape[0])
 
 
 if __name__ == '__main__':
-    path_la = r'C:\Users\Gustav\Desktop\MasterThesisResults\UCI_Laplace_MAP'
-    path_swag = r'C:\Users\Gustav\Desktop\MasterThesisResults\UCI_SWAG_MAP_nobayes'
-    plot_la_swag(path_la, path_swag)
+    # path_la = r'C:\Users\Gustav\Desktop\MasterThesisResults\UCI_Laplace_MAP'
+    # path_swag = r'C:\Users\Gustav\Desktop\MasterThesisResults\UCI_SWAG_MAP_nobayes'
+    # plot_la_swag(path_la, path_swag)
+    path = r'C:\Users\Gustav\Desktop\MasterThesisResults\UCI_Laplace_SWAG'
+
+    plot_la_swag_combined(path)
     breakpoint()
 
 
