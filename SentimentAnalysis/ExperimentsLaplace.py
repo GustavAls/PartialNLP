509553ef2d2110872ddb87c4c293e6bf6edf11a6
GustavAls <gustavals1998@gmail.com>
2023-11-23 14:12:21 +0100

    Experiments

--- SentimentAnalysis/ExperimentsLaplace.py
+++ SentimentAnalysis/ExperimentsLaplace.py
@@ -37,7 +37,7 @@ class LaplaceExperiments:
         self.default_args = {'output_path': args.output_path,
                              'train_batch_size': 32, 'eval_batch_size': 32, 'device': 'cpu', 'num_epochs': 1.0,
                              'dataset_name': 'imdb',
-                             'train': True, 'train_size': 1, 'test_size': 1, 'device_batch_size': 4,
+                             'train': True, 'train_size': 1, 'test_size': 1, 'device_batch_size': 2,
                              'learning_rate': 5e-05, 'seed': 0,'val_size': 1,
                              'laplace': True, 'swag': False, 'save_strategy': 'no',
                              'load_best_model_at_end': False, 'no_cuda': False}
