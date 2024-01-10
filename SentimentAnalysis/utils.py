abfc2db7781ebf941fba789aaaf115d8ecd8211f
PeterKampen <s183993@student.dtu.dk>
2024-01-10 14:15:57 +0100

    sublayer swag and some plotting stuff

--- SentimentAnalysis/utils.py
+++ SentimentAnalysis/utils.py
@@ -1191,6 +1191,33 @@ def find_diff_between_map_and_other(map_path, other_path, data_path, save_path =
         data_two.to_csv(os.path.join(save_path, 'wrongly_labeled_data_cls_two.txt'), index = False)
 
 
+def find_diff_between_map_and_other_swag(map_path, other_path, data_path):
+    data = pd.read_csv(data_path)
+    map_eval = pickle.load(open(map_path, 'rb'))
+    other_eval = pickle.load(open(other_path, 'rb'))
+    map_eval = map_eval['results']
+    softmax_ = nn.Softmax(dim=1)
+    best = other_eval[5]
+    labels_map = map_eval.labels
+    map_preds, best_preds = softmax_(map_eval.predictions).numpy(), best.predictions.numpy()
+    labels = labels_map.numpy()
+    labels_data = np.asarray(data['label'])
+    assert np.sum(labels - labels_data) == 0
+
+    data_cls = find_stuff_for_class_swag(labels, map_preds, best_preds, 0, np.array([data['sentence1'], data['sentence2']]))
+    return data_cls
+def find_stuff_for_class_swag(labels, preds_map, preds_swa, class_number, data):
+
+    data = data.T
+    correct_other = list(np.argwhere(preds_swa.argmax(-1) == labels).flatten())
+    correct_map = list(np.argwhere(preds_map.argmax(-1) == labels).flatten())
+    disjoint =  list(set(correct_other) - set(correct_map))
+    disjoint_labels = labels[disjoint]
+    highest_scoring = np.argsort(preds_swa[disjoint, disjoint_labels])[::-1]
+    data_cls = data[disjoint][highest_scoring]
+    return data_cls
+
+
 def find_stuff_for_class(labels, preds_map, preds_la, class_number, data):
 
     data = data.T
@@ -1289,10 +1316,10 @@ if __name__ == '__main__':
     # path = r'C:\Users\45292\Documents\Master\SentimentClassification\Laplace\random_ramping'
     # path = r'C:\Users\45292\Documents\Master\SentimentClassification\SWAG\random_ramping'
 
-    map_path = r"C:\Users\45292\Documents\Master\NLP\RTE\map\run_number_0_map.pkl"
-    other_path = r"C:\Users\45292\Documents\Master\NLP\RTE\laplace\nli_sublayer_full\run_0\run_number_0.pkl"
-    data_path =  r"C:\Users\45292\Documents\Master\NLP\RTE\Data\test_data.csv"
-    find_diff_between_map_and_other(map_path, other_path, data_path)
+    map_path = r"C:\Users\45292\Documents\Master\NLP\MRPC\map\run_number_0_map.pkl"
+    other_path = r"C:\Users\45292\Documents\Master\NLP\MRPC\swag\nli_random_ramping\run_0\run_number_0.pkl"
+    data_path =  r"C:\Users\45292\Documents\Master\NLP\MRPC\Data\test_data.csv"
+    find_diff_between_map_and_other_swag(map_path, other_path, data_path)
 
     breakpoint()
     # save_path = r'C:\Users\45292\Documents\Master\NLP\SST2\Figures\Examples\Data'
